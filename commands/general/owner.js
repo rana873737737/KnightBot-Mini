9f9c15c222a709f16f5a4e9a35b751501f8b2ac0
rana873737737 <shahinevuu@gmail.com>
2026-07-18 02:09:33 +0600

    Update owner.js

--- commands/general/owner.js
+++ commands/general/owner.js
@@ -40,7 +40,7 @@ END:VCARD
                 }
             });
 
-            await extra.reply('👑 Here is the contact of my *Owner*.');
+            await extra.reply('👑 Here is the contact of my *𝐎ᴡɴᴇʀ*.');
 
         } catch (error) {
             console.error('Owner command error:', error);
